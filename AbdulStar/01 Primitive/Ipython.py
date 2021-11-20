592895f847b38bed4fc480a6698aff0bdfcdc50d
Abdulstar Kousa <boody.koussa@gmail.com>
2021-11-20 22:59:31 +0100

    Update

--- AbdulStar/01 Primitive/Ipython.py
+++ AbdulStar/01 Primitive/Ipython.py
@@ -1,7 +1,7 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
+# # To add a new cell, type '# %%'
+# # To add a new markdown cell, type '# %% [markdown]'
 
-from IPython import get_ipython
-from IPython.display import display
-get_ipython().run_line_magic('matplotlib', 'inline') # %matplotlib inline
-get_ipython().run_line_magic('timeit', "df.groupby('Contry').count().collect()") # %timeit
+# from IPython import get_ipython
+# from IPython.display import display
+# get_ipython().run_line_magic('matplotlib', 'inline') # %matplotlib inline
+# get_ipython().run_line_magic('timeit', "df.groupby('Contry').count().collect()") # %timeit
